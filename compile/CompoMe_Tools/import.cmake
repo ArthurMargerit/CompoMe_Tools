aa399272c9200685dda389371ef63b6ca010eb49
ruhtra <ruhtra.mar@gmail.com>
2021-01-10 23:42:01 +0100

    add ARCHI master

--- compile/CompoMe_Tools/import.cmake
+++ compile/CompoMe_Tools/import.cmake
@@ -9,16 +9,16 @@ include("./compile/CompoMe_Tools/Ext_import.cmake" OPTIONAL)
 
 
 ###############################################################################
-list(APPEND _include_directories      "/home/ruhtra/compo/Test/gen/cpp_before/CompoMe_Log/inc")
-list(APPEND _include_directories_swig "/home/ruhtra/compo/Test/gen/cpp_before/CompoMe_Log/swig")
-include("/home/ruhtra/compo/Test/gen/cpp_before/CompoMe_Log/compile/CompoMe_Log/Ext_import.cmake" OPTIONAL)
+list(APPEND _include_directories      "/home/ruhtra/compo/build/CompoMe_Log/inc")
+list(APPEND _include_directories_swig "/home/ruhtra/compo/build/CompoMe_Log/swig")
+include("/home/ruhtra/compo/build/CompoMe_Log/compile/CompoMe_Log/Ext_import.cmake" OPTIONAL)
 
 
 
 ###############################################################################
-list(APPEND _include_directories      "/home/ruhtra/compo/Test/gen/cpp_before/CompoMe/inc")
-list(APPEND _include_directories_swig "/home/ruhtra/compo/Test/gen/cpp_before/CompoMe/swig")
-include("/home/ruhtra/compo/Test/gen/cpp_before/CompoMe/compile/CompoMe/Ext_import.cmake" OPTIONAL)
+list(APPEND _include_directories      "/home/ruhtra/compo/build/CompoMe/inc")
+list(APPEND _include_directories_swig "/home/ruhtra/compo/build/CompoMe/swig")
+include("/home/ruhtra/compo/build/CompoMe/compile/CompoMe/Ext_import.cmake" OPTIONAL)
 
 
 
--- compile/CompoMe_Tools/links.cmake
+++ compile/CompoMe_Tools/links.cmake
@@ -4,15 +4,15 @@ include("${CMAKE_CURRENT_LIST_DIR}/Ext_links.cmake" OPTIONAL)
 
 
 
-list(APPEND _links_dir "-L/home/ruhtra/compo/Test/gen/cpp_before/CompoMe_Log/lib")
+list(APPEND _links_dir "-L/home/ruhtra/compo/build/CompoMe_Log/lib")
 list(APPEND _links_lib "CompoMe_Log")
-include("/home/ruhtra/compo/Test/gen/cpp_before/CompoMe_Log/compile/CompoMe_Log/Ext_links.cmake" OPTIONAL)
+include("/home/ruhtra/compo/build/CompoMe_Log/compile/CompoMe_Log/Ext_links.cmake" OPTIONAL)
 
 
 
-list(APPEND _links_dir "-L/home/ruhtra/compo/Test/gen/cpp_before/CompoMe/lib")
+list(APPEND _links_dir "-L/home/ruhtra/compo/build/CompoMe/lib")
 list(APPEND _links_lib "CompoMe")
-include("/home/ruhtra/compo/Test/gen/cpp_before/CompoMe/compile/CompoMe/Ext_links.cmake" OPTIONAL)
+include("/home/ruhtra/compo/build/CompoMe/compile/CompoMe/Ext_links.cmake" OPTIONAL)
 
 
 

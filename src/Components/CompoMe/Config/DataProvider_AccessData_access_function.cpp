c74ce2ebd0fef12f7800f850831449bd1a3e1af2
tiregram <ruhtra.mar@gmail.com>
2021-02-28 20:22:43 +0100

    Revert "stream clean"
    This reverts commit 0af7dc265ee6ae8528cbe952d9c4679a95a86cd1.

--- src/Components/CompoMe/Config/DataProvider_AccessData_access_function.cpp
+++ src/Components/CompoMe/Config/DataProvider_AccessData_access_function.cpp
@@ -1,6 +1,5 @@
-#include "Components/CompoMe/Config/DataProvider_AccessData_access.hpp"
 #include "Components/CompoMe/Config/DataProvider.hpp"
-#include "CompoMe/Log.hpp"
+#include "Components/CompoMe/Config/DataProvider_AccessData_access.hpp"
 
 namespace CompoMe {
 
@@ -9,66 +8,48 @@ namespace Config {
 CompoMe::Config::Result_state
 DataProvider_AccessData_access::add(CompoMe::String key,
                                     CompoMe::String value) {
-  if(this->exist(key)) {
-    return Result_state::INVALID_KEY;
-  }
 
-  auto& values = this->get_c().a_values();
-  values[key] = value;
+  CompoMe::Config::Result_state ret =
 
-  return Result_state::VALID;
+      CompoMe::Config::Result_state();
+
+  return ret;
 }
 CompoMe::Config::Result_state
 DataProvider_AccessData_access::set(CompoMe::String key,
                                     CompoMe::String value) {
 
-  if(! this->exist(key)){
-    return Result_state::INVALID_KEY;
-  }
+  CompoMe::Config::Result_state ret =
 
-  auto& values = this->get_c().a_values();
-  C_INFO(key, " = ", value , "previous =", values[key]);
-  values[key]= value;
-  return Result_state::VALID;
+      CompoMe::Config::Result_state();
+
+  return ret;
 }
-
 CompoMe::Config::Result_state
 DataProvider_AccessData_access::del(CompoMe::String key) {
 
-  if(! this->exist(key)){
-    return Result_state::INVALID_KEY;
-  }
+  CompoMe::Config::Result_state ret =
 
-  this->get_c().a_values().erase(key);
-  return Result_state::VALID;
+      CompoMe::Config::Result_state();
+
+  return ret;
 }
-
 CompoMe::Config::Result_get
 DataProvider_AccessData_access::get(CompoMe::String key) {
 
-  CompoMe::Config::Result_get ret = CompoMe::Config::Result_get();
+  CompoMe::Config::Result_get ret =
 
-  if(! this->exist(key)){
-    ret.set_state(Result_state::INVALID_KEY);
-    return ret;
-  }
+      CompoMe::Config::Result_get();
 
-  auto& val = this->get_c().a_values();
-
-  auto r = val.find(key);
-  if( r == val.end()) {
-    ret.set_state(Result_state::INVALID_KEY);
-    return ret;
-  }
-
-  ret.set_state(Result_state::VALID);
-  ret.set_result((*r).second);
   return ret;
 }
-
 bool DataProvider_AccessData_access::exist(CompoMe::String key) {
-  auto& val = this->get_c().a_values();
-  return val.find(key) != val.end();
+
+  bool ret =
+
+      bool();
+
+  return ret;
 }
 
 } // namespace Config
